d92e6fdf4b735e1d23705a5fc009db901d617d8d
egorov.andrey34 <egorov.andrey34@wb.ru>
2023-08-27 20:37:44 +0300

    HW11: Initial commit

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+JENKINS_KEYFILE="$HOME/.ssh/jenkins_agent_key"
+JENKINS_PUBKEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFB3Rq/cwFMYwA4VKbdEpoaxB5Rz8kC04WA1rqLPiGwU jenkins"
+
+set -xe
+
+function install_docker() {
+    apt update
+    apt install --yes ca-certificates curl gnupg
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/$(. /etc/os-release && echo "$ID")/gpg \
+        | gpg --batch --yes --dearmor -o /etc/apt/keyrings/docker.gpg
+    chmod a+r /etc/apt/keyrings/docker.gpg
+    echo deb [arch=$(dpkg --print-architecture) \
+            signed-by=/etc/apt/keyrings/docker.gpg] \
+            https://download.docker.com/linux/$(. /etc/os-release && echo "$ID") \
+            $(. /etc/os-release && echo "$VERSION_CODENAME") stable \
+        > /etc/apt/sources.list.d/docker.list
+    apt update
+    apt install --yes docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+}
+
+case "$1" in
+    jenkins)
+        install_docker
+        pushd jenkins
+        docker compose up -d
+        if [[ ! -f "$JENKINS_KEYFILE" ]]; then
+            ssh-keygen -q -t ed25519 -f "$JENKINS_KEYFILE" -N ""
+        fi
+        ;;
+    agent)
+        install_docker
+        apt install --yes openjdk-11-jre-headless
+        if ! grep --fixed-strings --line-regexp "$JENKINS_PUBKEY" "/root/.ssh/authorized_keys" > /dev/null; then
+            echo "$JENKINS_PUBKEY" >> "/root/.ssh/authorized_keys"
+        fi
+        ;;
+    *)
+        echo "$0 jenkins|agent"
+        echo ""
+        ;;
+esac
